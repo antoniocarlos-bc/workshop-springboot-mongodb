f9571086760396b787ff179281ad59a7901b2944
antoniocarlos-bc <antoniocarlos_bc@outlook.com>
2021-03-18 23:48:14 -0300

    Obtendo usuário por id

--- src/main/java/com/antoniocrocha/workshopmongo/services/UserService.java
+++ src/main/java/com/antoniocrocha/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.antoniocrocha.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.antoniocrocha.workshopmongo.domain.User;
 import com.antoniocrocha.workshopmongo.repository.UserRepository;
+import com.antoniocrocha.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(()->new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
